7a16bd7746c63ccb19118956c3fda6e0e9d4035d
toltalchaos <59936738+toltalchaos@users.noreply.github.com>
2021-01-12 14:01:44 -0700

    added Albums entity

--- ChinookSoloution/ChinookSyatem/Entities/Album.cs
+++ ChinookSoloution/ChinookSyatem/Entities/Album.cs
@@ -10,7 +10,28 @@ using System.ComponentModel.DataAnnotations;
 namespace ChinookSyatem.Entities
 {
     [Table("Albums")]
-    class Album
+    internal class Album
     {
+        private string _ReleaseLabel;
+        [Key]
+        public int AlbumId { get; set; }
+
+        [Required(ErrorMessage = "Album Title is a required field")]
+        [StringLength(160, ErrorMessage ="album title is limited to 160 characters")]
+        public string Title { get; set; }
+
+        
+        //FK annotation not needed if same name
+        public int ArtistId { get; set; } //not int? which = not nullable
+
+        public int ReleaseYear { get; set; }
+
+        [StringLength(50, ErrorMessage = "_ReleaseLabel length is 50 charater maximum")]
+        public string ReleaseLabel
+        {
+            //coded as fully implamented 
+            get { return _ReleaseLabel; }
+            set { _ReleaseLabel = string.IsNullOrEmpty(value) ? null : value; }
+        }
     }
 }
