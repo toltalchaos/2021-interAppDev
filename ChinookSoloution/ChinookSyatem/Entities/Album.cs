e158493e4af79d3ff43e2272e5e5050ca9dea776
toltalchaos <59936738+toltalchaos@users.noreply.github.com>
2021-01-12 13:48:51 -0700

    added artists class

---
+++ ChinookSoloution/ChinookSyatem/Entities/Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#region additional-libraries
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+namespace ChinookSyatem.Entities
+{
+    [Table("Albums")]
+    class Album
+    {
+    }
+}
---
+++ ChinookSoloution/ChinookSyatem/Entities/Artist.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region additional-libraries
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+namespace ChinookSyatem.Entities
+{
+    [Table("Artists")]
+
+    //internal class to make restricted access within library
+    internal class Artist
+    {
+        private string _Name;
+
+
+        [Key]
+        public int ArtistID { get; set; }
+
+        [StringLength(120, ErrorMessage = "error limit of 120 characters for artist name.")]
+        public string Name { 
+            //coded as fully implamented 
+            get { return _Name; }
+            set { _Name = string.IsNullOrEmpty(value) ? null : value;}
+        }
+
+
+    }
+}
