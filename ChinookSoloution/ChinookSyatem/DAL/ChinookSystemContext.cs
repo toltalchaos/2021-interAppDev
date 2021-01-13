deb0107baae7f070cb051a9bd0afd9f734a0cb56
toltalchaos <59936738+toltalchaos@users.noreply.github.com>
2021-01-13 13:57:19 -0700

    added connection strings

---
+++ ChinookSoloution/ChinookSyatem/DAL/ChinookSystemContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#region additional namespaces
+using System.Data.Entity;
+using ChinookSyatem.Entities;
+#endregion
+namespace ChinookSyatem.DAL
+{
+    internal class ChinookSystemContext: DbContext //inheritance ties DB context
+    {
+        public ChinookSystemContext() : base("name=ChinookDB")
+        {
+
+        }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<MediaType> MediaTypes { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+    }
+}
