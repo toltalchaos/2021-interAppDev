d764f74db15a178a2e5614666b00d5cde0701d03
toltalchaos <59936738+toltalchaos@users.noreply.github.com>
2021-01-13 14:16:48 -0700

    new method to get ALL artist Albums

---
+++ ChinookSoloution/ChinookSyatem/BLL/AlbumController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#region additional namespaces
+using ChinookSyatem.DAL;
+using ChinookSyatem.Entities; //for SQL and are internal
+using ChinookSyatem.ViewModels; //for dataclasses to transfer data from BLL to webapp
+using System.ComponentModel; //for ODS Wizard
+#endregion
+namespace ChinookSyatem.BLL
+{
+    [DataObject]
+    public class AlbumController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ArtistAlbums> Albums_GetArtistAlbums()
+        {
+            using(var context = new ChinookSystemContext())
+            {
+                //select all albums and create new instance of viewmodel
+                IEnumerable<ArtistAlbums> results = from x in context.Albums
+                                                    select new ArtistAlbums
+                                                    {
+                                                        Title = x.Title,
+                                                        ReleaseYear = x.ReleaseYear,
+                                                        ArtistName = x.Artist.Name
+                                                    };
+                return results.ToList();
+            }
+        }
+
+
+    }
+}
