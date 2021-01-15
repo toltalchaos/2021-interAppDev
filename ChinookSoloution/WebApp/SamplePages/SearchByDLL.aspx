655daa8d8712f93fb937deef5e605c7e81898e5a
toltalchaos <59936738+toltalchaos@users.noreply.github.com>
2021-01-15 16:09:24 -0700

    added filter search with code behind

---
+++ ChinookSoloution/WebApp/SamplePages/SearchByDLL.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="Search by DLL" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SearchByDLL.aspx.cs" Inherits="WebApp.SamplePages.SearchByDLL" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h1>
+        Search Albums By Artist
+    </h1>
+    <%-- Search area --%>
+
+    <div class="row">
+        <div class="offset-3 ">
+
+            <asp:Label ID="Label1" runat="server" Text="Select an Artist"></asp:Label> &nbsp;&nbsp;
+
+            <asp:DropDownList ID="ArtistList" runat="server"></asp:DropDownList>&nbsp;&nbsp;
+
+            <asp:LinkButton ID="SearchAlbumsBTN" runat="server" OnClick="SearchAlbumsBTN_Click">Search<i class="fa fa-search"></i></asp:LinkButton>
+
+        </div>
+    </div>
+
+</asp:Content>
---
+++ ChinookSoloution/WebApp/SamplePages/SearchByDLL.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+#region additional namespaces
+using ChinookSystem.BLL; //controlllers
+using ChinookSystem.ViewModels; //models
+#endregion
+namespace WebApp.SamplePages
+{
+    public partial class SearchByDLL : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                //first page load
+
+                LoadArtistList();
+            }
+
+        }
+        protected void LoadArtistList()
+        {
+            ArtistController sysmgr = new ArtistController(); //new instance
+            List<SelectionList> info = sysmgr.Artist_DDLList(); //fill new list by call to query from new instance
+
+            //assume data collection needs to be sorted - could be done in BLL
+            // X and Y => do the following
+            info.Sort((x,y) => x.DisplayField.CompareTo(y.DisplayField)); //flip X and Y for opposite order
+
+            //setup the DDL 
+            ArtistList.DataSource = info;//attach data to object
+            //ArtistList.DataTextField = "DisplayField";
+            ArtistList.DataTextField = nameof(SelectionList.DisplayField); //info holds objects and selectionlist holds deffinitions
+            ArtistList.DataValueField = nameof(SelectionList.ValueField);
+            ArtistList.DataBind();//push into controller
+
+
+            //prompt line
+            ArtistList.Items.Insert(0, new ListItem("select...", "0")); //insert new item to DDL list
+
+        }
+
+        protected void SearchAlbumsBTN_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
